6b70ce5d050e1be25b5948d1f4cdbc699d6f3cc0
Kyle Hernandez <kylehernandez@Kyles-MBP.home>
2022-11-21 13:46:13 -0600

    71

--- dojo-blog3/src/views/HomeView.vue
+++ dojo-blog3/src/views/HomeView.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="home">
     home
-  </div>
+    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">click me</button>
+    </div>
 </template>
 
 <script>
 export default {
   name: "Home",
+  setup() {
+    console.log("setup");
+
+    let name = "kyle";
+    let age = 16;
+
+    const handleClick = () => {
+      console.log('you clicked me')
+    }
+
+    return { name, age, handleClick };
+  },
 };
 </script>
